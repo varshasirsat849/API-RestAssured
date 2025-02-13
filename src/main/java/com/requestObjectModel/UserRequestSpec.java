8122f6ee0dfbba8d5a9826255c924b447c1b8fb7
Roshsonwane11 <Sonawanerosh6@gmail.com>
2025-02-13 12:51:12 +0530

    Implemented POJO for Author | Implemented request specification for getAuthor and CreateAuthor

--- src/main/java/com/requestObjectModel/UserRequestSpec.java
+++ src/main/java/com/requestObjectModel/UserRequestSpec.java
@@ -1,18 +1,16 @@
 package com.requestObjectModel;
-
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 
-public class GetUsersRequestSpec {
+public class UserRequestSpec {
 	final static String baseURL = "https://fakerestapi.azurewebsites.net";
 	final static RequestSpecBuilder builder = new RequestSpecBuilder();
 
-	
-public RequestSpecification getUsers(){
-			
-		builder.setBaseUri(baseURL+"/api/v1/Users/");
-		builder.setBasePath("1");		
-		RequestSpecification reqSpec=builder.build();
+	public RequestSpecification getUsers() {
+
+		builder.setBaseUri(baseURL + "/api/v1/Users/");
+		builder.setBasePath("1");
+		RequestSpecification reqSpec = builder.build();
 		System.out.println(reqSpec);
 		return reqSpec;
 	}
